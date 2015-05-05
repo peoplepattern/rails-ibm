d067416abd7c3b0090495dc6164c7fdee7ba5f90
Daniel Zapata <daniel@peoplepattern.com>
2015-05-04 23:22:22 -0400

    posts + interests view temp'ed out

--- app/assets/javascripts/personModal.js
+++ app/assets/javascripts/personModal.js
@@ -20,22 +20,39 @@ $(function(){ // jquery on ready
     .controller('PersonModalController', PersonModalController)
     .filter('bigPic', bigPic);
 
+  ///////////////////////
+  // controllers
+  ///////////////////////
+
   PersonModalController.$inject = ['$scope'];
 
   function PersonModalController($scope){
     var vm = this;
     vm.person = {};
+    vm.posts = [{content: "blah"},{content: "blah2"}];
+    vm.interests = [{title: "sports"},{title: "stuff"}];
     activate();
 
     function activate(){
       angular.element(document).on('show.person', function(e){ //show.person events must contain 'person' field with necessary sub-data
         $scope.$applyAsync(function(){
           vm.person = e.person;
+          if (e.posts != undefined) {
+            vm.posts = posts;
+          }
+          if (e.interests != undefined) {
+            vm.interests = interests;
+          }
         });
         angular.element('#personModal').modal('show');
       });
     }
   };
+
+
+  /////////////////////
+  // filters
+  /////////////////////
 
   bigPic.$inject = [];
 
