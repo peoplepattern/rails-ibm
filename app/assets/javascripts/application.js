296c5a12d459c3f8cb8f073694364cf16df12a7f
Daniel Zapata <daniel@peoplepattern.com>
2015-05-04 21:27:23 -0400

    angular controlled modal! yippe

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require ./angular.min
 //= require_tree .
 
 //=require linkurious/sigma.require.js
---
+++ app/assets/javascripts/personModal.js
@@ -0,0 +1,50 @@
+// used for controlling when to show/hide the detailed person profile. Also controls the innerworkings/dynamic update of said modal.
+/*window.people = {}; //id-mapped list of people we have full profiles for
+$(function(){ // jquery on ready
+  $(document).on('show.person', function(e){ //show.person events must contain 'personID' fields
+    if (window.people[e.personID] != undefined){  // if person is in window.people, then don't get him from db
+      // show the modal for this person
+      showPerson(e.personID);
+    } else {                                      // else get him from db
+      //$.get();
+    }
+  });
+
+  // assumes id is in window.people;
+  function showPerson(id) {
+  }
+}); */
+
+(function(){
+  angular.module('personModal', [])
+    .controller('PersonModalController', PersonModalController)
+    .directive('ngToggler', ngToggler);
+
+  PersonModalController.$inject = ['$scope'];
+
+  function PersonModalController($scope){
+    var vm = this;
+    vm.person = {};
+    activate();
+
+    function activate(){
+      angular.element(document).on('show.person', function(e){ //show.person events must contain 'person' field with necessary sub-data
+        $scope.$applyAsync(function(){
+          vm.person = e.person;
+        });
+        angular.element('#personModal').modal('show');
+      });
+    }
+  };
+
+  ngToggler.$inject = [];
+
+  function ngToggler(){
+    return function ($scope, el, attrs){
+    
+    };
+  };
+
+  // boostrap the angular application which controls the modal
+//  angular.bootstrap(angular.element('#personModal'), ['personModal']);
+})();
