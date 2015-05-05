ba11cd365945654fc1eeea929d45fbe44471debc
Daniel Zapata <daniel@peoplepattern.com>
2015-05-04 21:52:53 -0400

    basic event listening

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -28,28 +28,43 @@ class WelcomeController < ApplicationController
              ],
       nodes: [
               {
-                name: 'test',
+                displayName: 'test',
                 label: 'test',
                 id: 1,
                 size: 1,
               },
               {
-                name: 'test2',
+                displayName: 'test2',
                 label: 'test2',
                 id: 2,
                 size: 2,
               },
               {
-                name: 'test3',
+                displayName: 'test3',
                 label: 'test3',
                 id: 3,
                 size: 3,
               },
               {
-                name: 'test4',
+                displayName: 'test4',
                 label: 'test4',
                 id: 4,
                 size: 4,
+                tid: "id:twitter:22389244",
+                summary: "Interaction Designer @ibmwatson, digital anthropologist, feminist, and game designer. @ITP_NYU alum 2014. Code liberator @codeliberation. Co-creator @facetscon.",
+                image: {url: "https://pbs.twimg.com/profile_images/590939493693140992/z6TaOaH0_normal.jpg"},
+                handle: "carolinesinders",
+                extensions: {
+                  location: "new orleans//brooklyn",
+                  favorites: 8872,
+                  screenName: "carolinesinders",
+                  followers: 884,
+                  posts: 13230,
+                },
+                displayName: "caroline sinders",
+                objectType: "page",
+                url: "http://t.co/hMDmdQqGEd",
+                "@timestamp" =>  1430790129500
               },
              ],
     }
